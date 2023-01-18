be6c7e55f0d66511337a5be74ac47f3d04915be7
mantu kumar ojha <72141924+th3kumar@users.noreply.github.com>
2023-01-18 13:55:53 +0530

    Time: 19 ms (52.07%), Space: 12.4 MB (90.17%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,33 @@
+/*
+
+intution 
+
+by seeing 
+( K frequent ) words 
+ we guess  solution might use minimum heap in it 
+ 
+ T.C = O(nlogn)
+ S.C = O(n)
+
+*/
+
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map <string, int> umap;
+			for (string s: words) umap[s]++;
+			priority_queue <pair<int, string>> pq;
+			for (auto it: umap) {
+				// minus sign used to build min heap 
+				pq.push({-it.second, it.first});
+				if(pq.size() > k) pq.pop();
+			}
+			vector <string> res;
+			while (k--) {
+				res.push_back(pq.top().second);
+				pq.pop();
+			}
+			reverse (res.begin(), res.end());
+			return res;
+    }
+};
